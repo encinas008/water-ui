e91d8aaaba22df4508c4c2c51863a0335eea485d
Rafael Encinas <encinas008@gmail.com>
2025-12-30 07:40:51 -0400

    ENH

--- src/app/shared/components/header/user-dropdown/user-dropdown.component.ts
+++ src/app/shared/components/header/user-dropdown/user-dropdown.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { DropdownComponent } from '../../ui/dropdown/dropdown.component';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { DropdownItemTwoComponent } from '../../ui/dropdown/dropdown-item/dropdown-item.component-two';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-user-dropdown',
   templateUrl: './user-dropdown.component.html',
-  imports:[CommonModule,RouterModule,DropdownComponent,DropdownItemTwoComponent]
+  imports: [CommonModule, RouterModule, DropdownComponent, DropdownItemTwoComponent]
 })
 export class UserDropdownComponent {
   isOpen = false;
+  userInfo: any = null;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.userInfo = this.authService.getUserInfo();
+  }
 
   toggleDropdown() {
     this.isOpen = !this.isOpen;
@@ -18,5 +29,11 @@ export class UserDropdownComponent {
 
   closeDropdown() {
     this.isOpen = false;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.closeDropdown();
+    this.router.navigate(['/signin']);
   }
 }
